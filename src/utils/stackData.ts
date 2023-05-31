18ffbb1230e5a56e7f69316eeb9d6540eda154f8
jaonunesunb <190109831@aluno.unb.br>
2023-05-31 08:04:44 -0300

    new stacks

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,6 +1,8 @@
 import imgHTML from "../public/static/img/stack/html.svg";
 import imgCSS from "../public/static/img/stack/css.svg";
+import imgPhp from "../public/static/img/stack/php.svg";
 import imgJS from "../public/static/img/stack/js.svg";
+import imgPython from "../public/static/img/stack/python.svg";
 import imgNode from "../public/static/img/stack/node.svg";
 import imgStyled from "../public/static/img/stack/styled.svg";
 import imgReact from "../public/static/img/stack/react.svg";
@@ -9,17 +11,18 @@ import { SiTypescript } from "react-icons/si";
 import { FaDocker } from "react-icons/fa";
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: imgHTML,
-  },
-  {
-    title: "CSS",
-    img: imgCSS,
-  },
+
   {
     title: "JS",
     img: imgJS,
+  },
+  {
+    title: "Python",
+    img: imgPython,
+  },
+  {
+    title: "PHP",
+    img: imgPhp,
   },
   {
     title: "Node JS",
